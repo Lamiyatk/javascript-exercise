bb19b0ae477bdb1ff23bd1346f2e9ecbb52345ec
lamiyatk <lamiya@bititude.com>
2024-08-23 17:56:31 +0530

    string added at front and back program

---
+++ char-front-back.js
@@ -0,0 +1,10 @@
+function addCharFrontAndBack(str){
+    let length = str.length;
+    if (length === 0){
+    }
+
+return (
+     str.charAt(0) + str.substring() +str.charAt(str.length -0) + str.charAt(0) );
+
+}
+console.log(addCharFrontAndBack("Hello"));
