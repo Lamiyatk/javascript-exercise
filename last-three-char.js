2f76bedbcfe9ec912145dfad81cb37fa3421e6e9
lamiyatk <lamiya@bititude.com>
2024-08-27 12:06:08 +0530

    last three charecters program

---
+++ last-three-char.js
@@ -0,0 +1,9 @@
+function LastThreeCharecter(str){
+    let length = str.length;
+    if (length <= 3){
+    }
+    return(
+    str.charAt(length-3) + str.charAt(length -2)+str.charAt(length - 1) + str.substring() + str.charAt(length-3)+str.charAt(length - 2)+ str.charAt(length -1));
+    
+}
+console.log(LastThreeCharecter("HelloWorld"));
